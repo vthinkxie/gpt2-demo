0a547dcbdbd03e34dc2e2fc03468086bb5a6587c
Yadong Xie <vthinkxie@gmail.com>
2023-11-25 14:30:14 +0800

    feat: add eslint

--- src/app/api/run/route.ts
+++ src/app/api/run/route.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from 'next/server';
-import type { NextRequest } from 'next/server';
+import type { NextRequest } from "next/server";
 export function POST(request: NextRequest) {
-  return fetch('https://zj6ril0k-mygpt2.bjz.edr.lepton.ai/run', {
-    method: 'POST', body: request.body, duplex: 'half', headers: {
-      "Authorization": "Bearer 455bd051-97b7-470f-a8ac-54543ee8f2ec"
-    }
-  } as RequestInit)
+  return fetch("http://localhost:8080/run", {
+    method: "POST",
+    body: request.body,
+    duplex: "half",
+    headers: {
+      Authorization: "Bearer 455bd051-97b7-470f-a8ac-54543ee8f2ec",
+    },
+  } as RequestInit);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,31 @@
-'use client';
-import {useEffect, useState} from "react";
-import useSWR from "swr";
+"use client";
+import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Home() {
-  const [text, setText] = useState('')
+  const [text, setText] = useState("");
+  const debounced = useDebouncedCallback((value) => {
+    fetch("/api/run", {
+      method: "POST",
+      body: JSON.stringify({
+        inputs: value,
+        max_new_tokens: 50,
+        do_sample: true,
+        top_k: 50,
+        top_p: 0.95,
+      }),
+    })
+      .then((r) => r.json())
+      .then((e) => setText(e));
+  }, 500);
   return (
     <div>
-      <input className="w-full" defaultValue="I enjoy walking with my cute dog" onChange={e=>    fetch('/api/run',{
-        method: "POST", body: JSON.stringify({
-          "inputs": e.target.value,
-          "max_new_tokens": 50,
-          "do_sample": true,
-          "top_k": 50,
-          "top_p": 0.95
-        })
-      }).then(r=>r.json()).then(e=>setText(e))} />
+      <input
+        className="w-full"
+        defaultValue="I enjoy walking with my cute dog"
+        onChange={(e) => debounced(e.target.value)}
+      />
       <p>{text}</p>
     </div>
-  )
+  );
 }
